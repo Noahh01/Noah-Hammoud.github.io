e39eaf2e7bb255f3920d67604ec7e37c9eace723
Noah Hammoud <noah.hammoud01@gmail.com>
2024-01-05 13:13:55 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const navLinks = document.querySelectorAll(".nav-item");
+
+const updateNavLink = () => {
+  let y = window.scrollY + window.innerHeight / 2;
+
+  navLinks.forEach(link => {
+    const id = link.getAttribute("scroll-to");
+    const section = document.getElementById(id);
+      
+    if (y >= section.offsetTop && y < section.offsetTop + section.offsetHeight) {
+        link.classList.add("active");
+    } else {
+        link.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener('scroll', updateNavLink);
